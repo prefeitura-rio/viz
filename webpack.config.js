d45aa1150a9b79d8e1c222533476934996565b5f
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-07-08 17:03:31 -0300

    feat: split components and add scroll example component

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,49 @@
-const path = require('path');
-const { webpack } = require('webpack');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            {
-                test: /\.(pdf|jpg|png|gif|svg|ico)$/,
-                use: [
-                    {
-                    loader: 'url-loader'
-                    },
-                ]
-            },
-            {  
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                loader: "file-loader"
-            }
+  entry: {
+    index: "./src/index.js",
+    scroll_index: "./src/components/scroll_example/scroll_example.js"
+    // add components here:
+    // component1: "./src/components/component1.js",
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        use: [
+          {
+            loader: "url-loader"
+          }
         ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist')
-        }
-    },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        loader: "file-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle_[name].js"
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist")
+    }
+  }
 };
