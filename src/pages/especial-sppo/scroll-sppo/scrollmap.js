4b3dbc6baefb1fd278010ac36cc655e74fc249fe
d116626 <d116626@gmail.com>
2022-07-10 00:14:38 -0300

    feat: try to update initial view state with scroll

--- src/pages/especial-sppo/scroll-sppo/scrollmap.js
+++ src/pages/especial-sppo/scroll-sppo/scrollmap.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+// https://stackoverflow.com/questions/70666753/how-to-update-progress
+import React, { Component, useState } from "react";
 import styled from "styled-components";
 import { Controller, Scene } from "react-scrollmagic";
 import TripsMap from "../tripsmap";
@@ -6,55 +7,62 @@ import TripsMap from "../tripsmap";
 const windowWidth = window.innerWidth;
 const const_duration = 400;
 
-export default class ScrollMap extends Component {
-  render() {
-    const TweenStyled = styled.div`
-      .section {
-        height: 50vh;
-        background-color: #87c5f5;
-      }
+export default function ScrollMap() {
+  const TweenStyled = styled.div`
+    .section {
+      height: 50vh;
+      background-color: #87c5f5;
+    }
 
-      .black-box {
-        height: 100px;
-        background-color: black;
-      }
+    .black-box {
+      height: 100px;
+      background-color: black;
+    }
 
-      .busao {
-        height: ${const_duration}px;
-        // height: 100vh;
-        background-color: #d5e8f7;
-      }
-    `;
+    .busao {
+      height: ${const_duration}px;
+      // height: 100vh;
+      background-color: #d5e8f7;
+    }
+  `;
+  const [currentZoom, setCurrentZoon] = useState(10);
 
-    return (
-      <TweenStyled>
+  // const setZoom = (progress) => {
+  //   if (progress > 0.5) {
+  //     return setCurrentZoon(12);
+  //   } else {
+  //     return setCurrentZoon(10);
+  //   }
+  // };
+
+  const updateZoom = (progress, initialZoom, finalZoom) => {
+    var step = finalZoom - initialZoom;
+    return initialZoom + step * progress;
+  };
+  return (
+    <TweenStyled>
+      <div className="section" />
+      <Controller>
+        {/* first scene */}
+        <div id="t1" />
+        <Scene triggerElement="#t1" duration={const_duration} indicators={true}>
+          {(progress, event) => (
+            <TripsMap
+              INITIAL_VIEW_STATE={{
+                longitude: updateZoom(progress, -43.45938, -43.2301558),
+                latitude: updateZoom(progress, -22.94232, -22.9121089),
+                zoom: updateZoom(progress, 10, 15),
+                pitch: updateZoom(progress, 7, 50),
+                bearing: updateZoom(progress, 0, 30),
+              }}
+            />
+          )}
+        </Scene>
+        <div className="black-box" />
         <div className="section" />
-        <Controller>
-          {/* first scene */}
-          <div id="t1" />
-          <Scene
-            triggerElement="#t1"
-            duration={const_duration}
-            indicators={true}
-          >
-            {(progress, event) => (
-              <TripsMap
-                INITIAL_VIEW_STATE={{
-                  longitude: -43.45938,
-                  latitude: -22.94232,
-                  zoom: 13 * progress,
-                  pitch: 7.0,
-                  bearing: 0.0,
-                }}
-              />
-            )}
-          </Scene>
-          <div className="black-box" />
-          <div className="section" />
-          <div className="section" />
-          <div className="section" />
-        </Controller>
-      </TweenStyled>
-    );
-  }
+        <div className="section" />
+        <div className="section" />
+      </Controller>
+    </TweenStyled>
+  );
 }
