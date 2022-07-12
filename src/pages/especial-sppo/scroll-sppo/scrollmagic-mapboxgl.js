2e71288d6c04b4241a9f64f431441d9a3cfabb93
d116626 <d116626@gmail.com>
2022-07-12 10:29:44 -0300

    fix: put some parameters

--- src/pages/especial-sppo/scroll-sppo/scrollmagic-mapboxgl.js
+++ src/pages/especial-sppo/scroll-sppo/scrollmagic-mapboxgl.js
@@ -5,6 +5,12 @@ import Map from "react-map-gl";
 
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoiZXNjcml0b3Jpb2RlZGFkb3MiLCJhIjoiY2t3bWdmcHpjMmJ2cTJucWJ4MGQ1Mm1kbiJ9.4hHJX-1pSevYoBbja7Pq4w";
+const MAP_STYLE = "mapbox://styles/escritoriodedados/cl5b8ea0s002915qtaaxvxz8b";
+
+const DURATION = 400;
+const OFFSET = -450;
+const TOP_SCENE = "-1vh";
+
 const cities = {
   sp: { lat: -23.5691, lon: -46.6474 },
   rj: { lat: -22.9121089, lon: -43.2301558 },
@@ -33,7 +39,7 @@ export default function ScrollMapboxGL() {
   const [settings, setSettings] = useState({
     scrollZoom: false,
     mapboxAccessToken: MAPBOX_TOKEN,
-    mapStyle: "mapbox://styles/escritoriodedados/cl5b8ea0s002915qtaaxvxz8b",
+    mapStyle: MAP_STYLE,
     style: {
       position: "fixed",
       top: "0",
@@ -41,13 +47,6 @@ export default function ScrollMapboxGL() {
       width: "100vw",
       height: "100vh",
     },
-    boxZoom: true,
-    dragRotate: true,
-    dragPan: true,
-    keyboard: true,
-    doubleClickZoom: true,
-    touchZoomRotate: true,
-    touchPitch: true,
   });
 
   const updateSettings = useCallback(
@@ -66,30 +65,33 @@ export default function ScrollMapboxGL() {
 
   return (
     <TweenStyled>
-      <div className="section" id="section" />
-      <Map interactive={false} ref={mapRef} initialViewState={initialViewState} {...settings} />
+      <div className="section" />
+      <Map
+        interactive={false}
+        ref={mapRef}
+        initialViewState={initialViewState}
+        {...settings}
+      />
       <Controller>
         <Scene
           triggerElement="#t1"
           indicators={true}
-          pin={{
-            pushFollowers: false,
-          }}
-          duration={400}
+          pin={true}
+          duration={DURATION}
+          offset={OFFSET}
         >
           {(progress, event) => (
-            <h1 style={{ color: "#FFF", top: "28vh" }}>
+            <h1 style={{ color: "#FFF", top: TOP_SCENE }}>
               {" Scene 1 | Progress: "}
               {progress}
-              {
-              event.type === "enter" &&
-              flyToNextStep({
-                center: [cities.sp.lon, cities.sp.lat],
-                zoom: 9,
-                pitch: 0,
-                bearing: 0,
-                duration: 1000,
-              })}
+              {event.type === "enter" &&
+                flyToNextStep({
+                  center: [cities.sp.lon, cities.sp.lat],
+                  zoom: 9,
+                  pitch: 0,
+                  bearing: 0,
+                  duration: 1000,
+                })}
             </h1>
           )}
         </Scene>
@@ -100,21 +102,21 @@ export default function ScrollMapboxGL() {
           pin={{
             pushFollowers: false,
           }}
-          duration={400}
+          duration={DURATION}
+          offset={OFFSET}
         >
           {(progress, event) => (
-            <h1 style={{ color: "#FFF", top: "28vh" }}>
+            <h1 style={{ color: "#FFF", top: TOP_SCENE }}>
               {" Scene 2 | Progress: "}
               {progress}
-              {
-              event.type === "enter" &&
-              flyToNextStep({
-                center: [cities.rj.lon, cities.rj.lat],
-                zoom: 9,
-                pitch: 40,
-                bearing: 180,
-                duration: 2000,
-              })}
+              {event.type === "enter" &&
+                flyToNextStep({
+                  center: [cities.rj.lon, cities.rj.lat],
+                  zoom: 9,
+                  pitch: 40,
+                  bearing: 180,
+                  duration: 2000,
+                })}
             </h1>
           )}
         </Scene>
@@ -125,21 +127,21 @@ export default function ScrollMapboxGL() {
           pin={{
             pushFollowers: false,
           }}
-          duration={400}
+          duration={DURATION}
+          offset={OFFSET}
         >
           {(progress, event) => (
-            <h1 style={{ color: "#FFF", top: "28vh" }}>
+            <h1 style={{ color: "#FFF", top: TOP_SCENE }}>
               {" Scene 3 | Progress: "}
               {progress}
-              {
-              event.type === "enter" &&
-              flyToNextStep({
-                center: [cities.rj.lon, cities.rj.lat],
-                zoom: 10,
-                pitch: 30,
-                bearing: 0,
-                duration: 2000,
-              })}
+              {event.type === "enter" &&
+                flyToNextStep({
+                  center: [cities.rj.lon, cities.rj.lat],
+                  zoom: 10,
+                  pitch: 30,
+                  bearing: 0,
+                  duration: 2000,
+                })}
             </h1>
           )}
         </Scene>
@@ -150,20 +152,20 @@ export default function ScrollMapboxGL() {
           pin={{
             pushFollowers: false,
           }}
-          duration={400}
+          duration={DURATION}
+          offset={OFFSET}
         >
           {(progress, event) => (
-            <h1 style={{ color: "#FFF", top: "28vh" }}>
+            <h1 style={{ color: "#FFF", top: TOP_SCENE }}>
               {" Scene 4 | Progress: "}
               {progress}
-              {
-              event.type === "enter" &&
-              flyToNextStep({
-                center: [cities.rj.lon, cities.rj.lat],
-                zoom: 11,
-                pitch: 0,
-                duration: 2000,
-              })}
+              {event.type === "enter" &&
+                flyToNextStep({
+                  center: [cities.rj.lon, cities.rj.lat],
+                  zoom: 11,
+                  pitch: 0,
+                  duration: 2000,
+                })}
             </h1>
           )}
         </Scene>
@@ -174,28 +176,27 @@ export default function ScrollMapboxGL() {
           pin={{
             pushFollowers: false,
           }}
-          duration={400}
+          duration={DURATION}
+          offset={OFFSET}
         >
           {(progress, event) => (
-            <h1 style={{ color: "#FFF", top: "28vh" }}>
+            <h1 style={{ color: "#FFF", top: TOP_SCENE }}>
               {" Scene 5 | Progress: "}
               {progress}
-              {
-              event.type === "enter" &&
-              flyToNextStep({
-                center: [cities.sp.lon, cities.sp.lat],
-                zoom: 11,
-                pitch: 0,
-                bearing: 0,
-                duration: 2000,
-              })}
+              {event.type === "enter" &&
+                flyToNextStep({
+                  center: [cities.sp.lon, cities.sp.lat],
+                  zoom: 11,
+                  pitch: 0,
+                  bearing: 0,
+                  duration: 2000,
+                })}
             </h1>
           )}
         </Scene>
-        <div id="t5" className="section" />
-        <div className="section" />
+        <div id="t5" />
       </Controller>
-      <div className="section" />{" "}
+      <div className="section" />
     </TweenStyled>
   );
 }
