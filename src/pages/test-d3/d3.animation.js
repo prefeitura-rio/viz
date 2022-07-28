bc3f054ee389acad461fa7ceed5317f093ecf7ab
d116626 <d116626@gmail.com>
2022-07-28 12:55:21 -0300

    feat: first d3 chart

--- src/pages/test-d3/d3.animation.js
+++ src/pages/test-d3/d3.animation.js
@@ -8,14 +8,34 @@ class BarChart extends Component {
   }
 
   componentDidMount() {
-    let acessToRef = d3.select(this.myRef.current);
-    acessToRef.style("background-color", "green");
+    let chartRef = d3.select(this.myRef.current);
+    const w = 500;
+    const h = 400;
+
+    const data = [2, 15, 22, 9, 30, 20, 22, 45];
+
+    chartRef
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h)
+      .style("background", "#f0f0f0")
+      .style("padding", "10px")
+      .style("margin-left", "50px")
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 65)
+      .attr("y", (d, i) => h - 10 * d)
+      .attr("width", 35)
+      .attr("height", (d, i) => d * 10)
+      .attr("fill", (d, i) => (d >= 20 ? "red" : "green"));
   }
 
   render() {
     return (
       <>
-        <div ref={this.myRef}>Testing Ref</div>
+        <div ref={this.myRef}></div>
       </>
     );
   }
