859f8906b329ec9bbdd708948539f381d774f3e6
d116626 <d116626@gmail.com>
2022-07-29 19:19:12 -0300

    fix: rool back mapCSS

--- src/pages/subsidio-sppo/story.js
+++ src/pages/subsidio-sppo/story.js
@@ -124,8 +124,9 @@ export default function SubsidioSPPO() {
           position: "fixed",
           top: "0",
           left: "0",
-          // width: "100vw",
-          // height: "100vh",
+          width: "100vw",
+          height: "100vh",
+          // maxWidth: "100%",
           zIndex: "-1",
         }}
         animationLoopLength={21600}
