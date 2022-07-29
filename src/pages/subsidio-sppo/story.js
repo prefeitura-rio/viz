3fe977bbda8e20ec8f1ec657ec61ae2757e964a0
d116626 <d116626@gmail.com>
2022-07-29 18:56:50 -0300

    fix: zoom

--- src/pages/subsidio-sppo/story.js
+++ src/pages/subsidio-sppo/story.js
@@ -124,8 +124,8 @@ export default function SubsidioSPPO() {
           position: "fixed",
           top: "0",
           left: "0",
-          width: "100vw",
-          height: "100vh",
+          // width: "100vw",
+          // height: "100vh",
           zIndex: "-1",
         }}
         animationLoopLength={21600}
