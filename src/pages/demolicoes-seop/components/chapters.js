790c3fe063d92264715120acebdb2e4a6dcf9b6b
d116626 <d116626@gmail.com>
2022-08-08 13:19:40 -0300

    feat: migrate to tailwind for star responsive layots

--- src/pages/demolicoes-seop/components/chapters.js
+++ src/pages/demolicoes-seop/components/chapters.js
@@ -22,185 +22,6 @@ import CustomEase from "gsap/CustomEase";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-export const Logo = styled.img`
-  width: 150px;
-  // background-color: #01baef;
-  margin-bottom: auto;
-  margin-top: 2%;
-  grid-area: logo;
-`;
-
-export const NewsImage = styled.img`
-  width: 100%;
-  // background-color: #01baef;
-  margin-bottom: auto;
-  margin-top: 2%;
-  grid-area: logo;
-  margin-bottom: 30px;
-`;
-
-// export const CapaImg = styled.img`
-//   width: 100%;
-//   height: 100%;
-// `;
-
-const Title = styled.div`
-  width: 80%;
-  color: #406882;
-  font-family: "Poppins", serif;
-  font-weight: 700;
-  font-size: 7rem;
-  line-height: 1;
-  text-align: right;
-  grid-area: titulo;
-`;
-
-const AuthorText = styled.div`
-  width: 80%;
-  color: #406882;
-  font-family: "Poppins", serif;
-  font-weight: 500;
-  font-size: 1rem;
-  line-height: 1.3;
-  margin-bottom: auto;
-  margin-top: 30px;
-  text-align: right;
-  grid-area: subtitulo;
-`;
-
-const IntroTitle = styled.div`
-  grid-area: titulo;
-  color: #ffffff;
-  font-family: "Redaction", sans-serif;
-  font-weight: 700;
-  font-size: 2.7rem;
-  line-height: 1;
-  text-align: right;
-  opacity: 1;
-  align-self: end;
-  z-index: 999;
-`;
-
-const BigNumber = styled.div`
-  color: #1a374d;
-  font-family: "Redaction", sans-serif;
-  font-weight: 700;
-  font-size: 8rem;
-  line-height: 0.8;
-  opacity: 1;
-  align-self: end;
-`;
-
-const BigNumber1 = styled.div`
-  color: #1a374d;
-  font-family: "Redaction", sans-serif;
-  font-weight: 700;
-  font-size: 5rem;
-  line-height: 1;
-  opacity: 1;
-  align-self: end;
-`;
-
-const IntroText = styled.div`
-  grid-area: texto;
-  text-align: justify;
-  text-align-last: right;
-  /* direction: rtl; */
-  margin-bottom: 30%;
-  vertical-align: middle;
-  color: #ffffff;
-  font-family: "Redaction", sans-serif;
-  font-size: 1.2rem;
-  line-height: 1.5;
-  opacity: 1;
-`;
-
-const TextOrange = styled.div`
-  background: #c36a2d;
-  color: #fff;
-  display: inline;
-  padding: 1px 8px 1px 8px;
-  margin: 3px;
-`;
-
-const TextBlue = styled.div`
-  background: #1a374d;
-  color: #fff;
-  display: inline;
-  /* padding: 2px 8px 2px 8px; */
-  /* margin: 4px; */
-`;
-
-const ContainerCard = styled.div`
-  min-width: 250px;
-  margin: auto;
-  width: 40%;
-  padding: 40px;
-  background-color: rgba(0, 0, 0, 0.7);
-  opacity: 1;
-  border-radius: 0px;
-  backdrop-filter: blur(4px);
-`;
-
-const ContainerCard1 = styled.div`
-  min-width: 250px;
-  margin: auto auto auto 250px;
-  width: 25%;
-  padding: 40px;
-  background-color: rgba(0, 0, 0, 0.2);
-  opacity: 1;
-  border-radius: 0px;
-  backdrop-filter: blur(4px);
-`;
-
-const TextCard = styled.div`
-  color: #dbdbdb;
-  font-family: "Redaction", sans-serif;
-  font-size: 1.2rem;
-  line-height: 1.5;
-  text-align: left;
-`;
-
-const TextCard1 = styled.div`
-  color: #dbdbdb;
-  font-family: "Times New Roman", Times, serif;
-  font-size: 1.3rem;
-  line-height: 1.4;
-  margin-top: 0px;
-  margin-bottom: 0px;
-  text-align: left;
-`;
-
-const NewsTitle = styled.div`
-  color: #ffffff;
-  font-family: "Redaction", sans-serif;
-  font-weight: 700;
-  font-size: 2.2rem;
-  line-height: 1;
-  margin-bottom: 15px;
-  /* text-align: right;
-  opacity: 1;
-  align-self: end; */
-`;
-
-const Grid = styled.div`
-  display: block;
-  gap: 0px 30px;
-`;
-
-const Grid1 = styled.div`
-  display: flex;
-  aling-items: end;
-  justify-content: start;
-  margin-bottom: 20px;
-  gap: 0px 20px;
-`;
-
-const GifCapa = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
 export function Capa(
   props = {
     id: "",
@@ -210,14 +31,14 @@ export function Capa(
   props = setDefaultProps(props);
   return (
     <styles.CapaDiv id={props.id} ref={props.chapRef}>
-      <Logo src={logo}></Logo>
-      <Title>
+      <img src={logo} className="w-24 mt-[2vh]  lg:w-10"></img>
+      <styles.Title>
         1000 <br /> DEMOLIÇÕES
-      </Title>
-      <AuthorText>
+      </styles.Title>
+      <styles.AuthorText>
         Desenvolvido por <br />
         <a
-          style={{ fontSize: "1rem", fontWeight: "700", color: "#406882" }}
+          className="font-bold"
           href="https://www.dados.rio/"
           target="_blank"
           rel="noreferrer"
@@ -226,18 +47,14 @@ export function Capa(
         </a>{" "}
         <br />
         <a
-          style={{
-            fontSize: "1rem",
-            fontWeight: "700",
-            color: "#406882",
-          }}
+          className="font-bold"
           href="http://www.rio.rj.gov.br/web/seop"
           target="_blank"
           rel="noreferrer"
         >
           Secretaria Municipal de Ordem Pública
         </a>
-      </AuthorText>
+      </styles.AuthorText>
     </styles.CapaDiv>
   );
 }
@@ -252,15 +69,15 @@ export function Intro(
 
   return (
     <styles.IntroDiv ref={props.chapRef} id={props.id}>
-      <IntroTitle id={"intro"}>Como tudo começou</IntroTitle>
-      <IntroText>
+      <styles.IntroTitle id={"intro"}>Como tudo começou</styles.IntroTitle>
+      <styles.IntroText>
         A proliferação de construções irregulares é um problema que tomou conta
         da cidade do Rio há algumas décadas. Em áreas sob influência do crime
         organizado a preocupação é ainda maior. Estudos apontam que{" "}
-        <TextBlue>
+        <styles.TextBlue>
           a atividade imobiliária se tornou um dos principais sustentáculos
           financeiros de alguns grupos criminosos (HIRATA, 2021).
-        </TextBlue>{" "}
+        </styles.TextBlue>{" "}
         Essas construções são realizadas sem autorização da Prefeitura, em
         alguns casos em área pública e erguidas, muitas vezes, sem nenhum
         acompanhamento técnico. Não à toa a cidade já presenciou desastres
@@ -270,19 +87,19 @@ export function Intro(
         Exatamente para combater o crescimento desordenado da cidade, preservar
         vidas e asfixiar financeiramente o crime organizado que, desde meados de
         2021,{" "}
-        <TextBlue>
+        <styles.TextBlue>
           a Secretaria de Ordem Pública tem realizado operações constantes de
           demolição dessas construções sem regularização.
-        </TextBlue>{" "}
+        </styles.TextBlue>{" "}
         De lá para cá, foram 1.000 demolições! Isso mesmo, MIL construções
         irregulares postas abaixo em um ano e meio de trabalho.
         <br /> <br />
         Aqui cabe um destaque: o foco das operações é em{" "}
-        <TextBlue>
+        <styles.TextBlue>
           imóveis não habitados, erguidos de forma ilegal e que tragam risco à
           população.
-        </TextBlue>
-      </IntroText>
+        </styles.TextBlue>
+      </styles.IntroText>
     </styles.IntroDiv>
   );
 }
@@ -295,31 +112,31 @@ export function Prejuizo(
 ) {
   return (
     <styles.ChapterGenericDiv ref={props.chapRef} id={props.id}>
-      <ContainerCard>
-        <Grid>
-          <Grid1>
-            <BigNumber id="number">0</BigNumber>
-            <BigNumber1>milhões</BigNumber1>
-          </Grid1>
-          <TextCard>
+      <styles.ContainerCard>
+        <styles.Grid>
+          <styles.Grid1>
+            <styles.BigNumber id="number">0</styles.BigNumber>
+            <styles.BigNumber1>milhões</styles.BigNumber1>
+          </styles.Grid1>
+          <styles.TextCard>
             Por trás de tanta construção ilegal em área sob influência do crime
             organizado, há um grande interesse econômico. Estudos apontam que{" "}
-            <TextBlue>
+            <styles.TextBlue>
               os grupos criminais, principalmente as milícias, obtiveram um
               aumento de seus ganhos através do mercado imobiliário,
-            </TextBlue>{" "}
+            </styles.TextBlue>{" "}
             a partir da oferta e da proteção para execução de construções
             irregulares (HIRATA, 2021).
             <br /> <br />
             E, muito também, para atingi-los no bolso é que a SEOP tem realizado
             suas operações com foco nessas regiões. Prova disso é que{" "}
-            <TextBlue>
+            <styles.TextBlue>
               desde 2021 as operações da secretaria acarretaram em um prejuízo
               estimado em R$ xxx milhões para os cofres do crime organizado.
-            </TextBlue>
-          </TextCard>
-        </Grid>
-      </ContainerCard>
+            </styles.TextBlue>
+          </styles.TextCard>
+        </styles.Grid>
+      </styles.ContainerCard>
     </styles.ChapterGenericDiv>
   );
 }
@@ -334,7 +151,7 @@ export function Grafico(
 
   return (
     <styles.ChapterGenericDiv ref={props.chapRef} id={props.id}>
-      <ContainerCard>
+      <styles.ContainerCard>
         {/* <iframe
           title="1000 demolições desde o início da gestão"
           aria-label="Gráfico de coluna"
@@ -347,17 +164,19 @@ export function Grafico(
         ></iframe> */}
         {/* <script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(e){if(void 0!==e.data["datawrapper-height"]){var t=document.querySelectorAll("iframe");for(var a in e.data["datawrapper-height"])for(var r=0;r<t.length;r++){if(t[r].contentWindow===e.source)t[r].style.height=e.data["datawrapper-height"][a]+"px"}}}))}();
 </script> */}
-        <NewsImage src={grafico}></NewsImage>
-        <TextCard>
+        <styles.NewsImage src={grafico}></styles.NewsImage>
+        <styles.TextCard>
           Analisando as prioridades da Secretaria de Ordem Pública e da própria
           Prefeitura, dá para notar que o tema construções irregulares é tratado
-          com a <TextBlue>absoluta relevância</TextBlue> que o assunto merece.
-          Se olharmos historicamente, porém, nem sempre a análise foi essa. Na
-          gestão anterior foram feitas 359 ao longo de quatro anos. Desde que a
-          atual gestão assumiu{" "}
-          <TextBlue>1.000 demolições em apenas um ano e meio.</TextBlue>
-        </TextCard>
-      </ContainerCard>
+          com a <styles.TextBlue>absoluta relevância</styles.TextBlue> que o
+          assunto merece. Se olharmos historicamente, porém, nem sempre a
+          análise foi essa. Na gestão anterior foram feitas 359 ao longo de
+          quatro anos. Desde que a atual gestão assumiu{" "}
+          <styles.TextBlue>
+            1.000 demolições em apenas um ano e meio.
+          </styles.TextBlue>
+        </styles.TextCard>
+      </styles.ContainerCard>
     </styles.ChapterGenericDiv>
   );
 }
@@ -372,9 +191,9 @@ export function Exemplo1(
 
   return (
     <styles.ChapterGenericDiv ref={props.chapRef} id={props.id}>
-      <ContainerCard1>
-        <NewsImage src={riodaspedras}></NewsImage>
-        <NewsTitle>
+      <styles.ContainerCard1>
+        <styles.NewsImage src={riodaspedras}></styles.NewsImage>
+        <styles.NewsTitle>
           <a
             style={{
               fontSize: "2.2rem",
@@ -388,13 +207,13 @@ export function Exemplo1(
             Prédio desaba em comunidade dominada por milicianos no Rio de
             Janeiro
           </a>
-        </NewsTitle>
-        <TextCard1>
+        </styles.NewsTitle>
+        <styles.TextCard1>
           Criança e pai foram localizados mortos sob escombros após nove horas
           de buscas em Rio das Pedras, na Zona Oeste; quatro adultos foram
           resgatados.
-        </TextCard1>
-      </ContainerCard1>
+        </styles.TextCard1>
+      </styles.ContainerCard1>
     </styles.ChapterGenericDiv>
   );
 }
@@ -409,9 +228,9 @@ export function Exemplo2(
 
   return (
     <styles.ChapterGenericDiv ref={props.chapRef} id={props.id}>
-      <ContainerCard1>
-        <NewsImage src={muzema}></NewsImage>
-        <NewsTitle>
+      <styles.ContainerCard1>
+        <styles.NewsImage src={muzema}></styles.NewsImage>
+        <styles.NewsTitle>
           <a
             style={{
               fontSize: "2.2rem",
@@ -424,13 +243,13 @@ export function Exemplo2(
           >
             Morre a 24ª vítima do desabamento na Muzema
           </a>
-        </NewsTitle>
-        <TextCard1>
+        </styles.NewsTitle>
+        <styles.TextCard1>
           Adilma Rodrigues, de 35 anos, era casada com o pastor Cláudio,
           primeira vítima da queda dos prédios. Os dois prédios que caíram dia
           12 após fortes chuvas eram irregulares.
-        </TextCard1>
-      </ContainerCard1>
+        </styles.TextCard1>
+      </styles.ContainerCard1>
     </styles.ChapterGenericDiv>
   );
 }
@@ -445,9 +264,9 @@ export function Exemplo3(
 
   return (
     <styles.ChapterGenericDiv ref={props.chapRef} id={props.id}>
-      <ContainerCard1>
-        <NewsImage src={salgueiro}></NewsImage>
-        <NewsTitle>
+      <styles.ContainerCard1>
+        <styles.NewsImage src={salgueiro}></styles.NewsImage>
+        <styles.NewsTitle>
           <a
             style={{
               fontSize: "2.2rem",
@@ -460,13 +279,13 @@ export function Exemplo3(
           >
             Prédio de 4 andares desaba no Rio e deixa um morto e três feridos
           </a>
-        </NewsTitle>
-        <TextCard1>
+        </styles.NewsTitle>
+        <styles.TextCard1>
           Um prédio de quatro andares desabou no Morro do Salgueiro, na zona
           norte do Rio de Janeiro, nesta quarta-feira (17) e deixou um morto e
           três feridos, de acordo com o Corpo de Bombeiros
-        </TextCard1>
-      </ContainerCard1>
+        </styles.TextCard1>
+      </styles.ContainerCard1>
     </styles.ChapterGenericDiv>
   );
 }
@@ -481,9 +300,9 @@ export function Seop1(
 
   return (
     <styles.ChapterGenericDiv ref={props.chapRef} id={props.id}>
-      <ContainerCard1>
-        <NewsImage src={seop1}></NewsImage>
-        <NewsTitle>
+      <styles.ContainerCard1>
+        <styles.NewsImage src={seop1}></styles.NewsImage>
+        <styles.NewsTitle>
           <a
             style={{
               fontSize: "2.2rem",
@@ -496,12 +315,12 @@ export function Seop1(
           >
             Construção irregular é demolida na Muzema
           </a>
-        </NewsTitle>
-        <TextCard1>
+        </styles.NewsTitle>
+        <styles.TextCard1>
           Força-tarefa demole prédio de três andares que fica a menos de 100
           metros do condomínio que desabou, em 2019, deixando 24 mortos
-        </TextCard1>
-      </ContainerCard1>
+        </styles.TextCard1>
+      </styles.ContainerCard1>
     </styles.ChapterGenericDiv>
   );
 }
@@ -516,9 +335,9 @@ export function Seop2(
 
   return (
     <styles.ChapterGenericDiv ref={props.chapRef} id={props.id}>
-      <ContainerCard1>
-        <NewsImage src={seop2}></NewsImage>
-        <NewsTitle>
+      <styles.ContainerCard1>
+        <styles.NewsImage src={seop2}></styles.NewsImage>
+        <styles.NewsTitle>
           <a
             style={{
               fontSize: "2.2rem",
@@ -532,9 +351,11 @@ export function Seop2(
             Prédio irregular é demolido pela prefeitura, no Recreio dos
             Bandeirantes, na Zona Oeste
           </a>
-        </NewsTitle>
-        <TextCard1>Liminar impediu demolição no dia 25 de abril.</TextCard1>
-      </ContainerCard1>
+        </styles.NewsTitle>
+        <styles.TextCard1>
+          Liminar impediu demolição no dia 25 de abril.
+        </styles.TextCard1>
+      </styles.ContainerCard1>
     </styles.ChapterGenericDiv>
   );
 }
@@ -549,17 +370,17 @@ export function Fim(
 
   return (
     <styles.IntroDiv ref={props.chapRef} id={props.id}>
-      <IntroTitle id={"fimTitle"}>Fim</IntroTitle>
-      <IntroText>
+      <styles.IntroTitle id={"fimTitle"}>Fim</styles.IntroTitle>
+      <styles.IntroText>
         É inegável que o marco de 1.000 demolições, em apenas um ano e meio de
         gestão, é extremamente importante. Mostra que estamos atuantes e que
         seguiremos firmes no combate a ocupação desordenada da cidade, na
         proteção de vidas e em asfixiar financeiramente o crime organizado. Isso
         tudo só reforça a convicção que o trabalho precisa seguir .{" "}
-        <TextBlue>
+        <styles.TextBlue>
           Por isso, a Secretaria de Ordem Pública permanecerá atenta, colocando
           abaixo aquelas estruturas erguidas irregularmente.
-        </TextBlue>{" "}
+        </styles.TextBlue>{" "}
         Além disso, a secretaria entende a importância de ter uma ação cada vez
         mais estratégica, que saiba apontar prioridades e aja nos pontos mais
         sensíveis da cidade. Por isso a SEOP está investindo na melhoria da sua
@@ -567,7 +388,7 @@ export function Fim(
         apuração refinada das denúncias feitas pela população, como também por
         meios de novas tecnologias, que incluem drones, ortofotos e imagens de
         satélites.
-      </IntroText>
+      </styles.IntroText>
     </styles.IntroDiv>
   );
 }
@@ -582,17 +403,19 @@ export function Acompanhe(
 
   return (
     <styles.IntroDiv ref={props.chapRef} id={props.id}>
-      <IntroTitle id={"acompanheTitle"}>Acompanhe as ações da SEOP</IntroTitle>
-      <IntroText>
+      <styles.IntroTitle id={"acompanheTitle"}>
+        Acompanhe as ações da SEOP
+      </styles.IntroTitle>
+      <styles.IntroText>
         É inegável que o marco de 1.000 demolições, em apenas um ano e meio de
         gestão, é extremamente importante. Mostra que estamos atuantes e que
         seguiremos firmes no combate a ocupação desordenada da cidade, na
         proteção de vidas e em asfixiar financeiramente o crime organizado. Isso
         tudo só reforça a convicção que o trabalho precisa seguir .{" "}
-        <TextBlue>
+        <styles.TextBlue>
           Por isso, a Secretaria de Ordem Pública permanecerá atenta, colocando
           abaixo aquelas estruturas erguidas irregularmente.
-        </TextBlue>{" "}
+        </styles.TextBlue>{" "}
         Além disso, a secretaria entende a importância de ter uma ação cada vez
         mais estratégica, que saiba apontar prioridades e aja nos pontos mais
         sensíveis da cidade. Por isso a SEOP está investindo na melhoria da sua
@@ -600,7 +423,7 @@ export function Acompanhe(
         apuração refinada das denúncias feitas pela população, como também por
         meios de novas tecnologias, que incluem drones, ortofotos e imagens de
         satélites.
-      </IntroText>
+      </styles.IntroText>
     </styles.IntroDiv>
   );
 }
@@ -615,17 +438,18 @@ export function Oeste(
 
   return (
     <styles.IntroDiv ref={props.chapRef} id={props.id}>
-      <IntroTitle id={"zonaOesteTitle"}>
+      <styles.IntroTitle id={"zonaOesteTitle"}>
         Onde atuamos e o foco na Zona Oeste
-      </IntroTitle>
-      <IntroText>
+      </styles.IntroTitle>
+      <styles.IntroText>
         Apesar de ocorrerem em todas as regiões da cidade, as construções
-        irregulares têm uma <TextBlue>incidência maior na Zona Oeste</TextBlue>{" "}
-        do Município, onde ocorrem cerca de <TextBlue>66%</TextBlue> das
-        demolições realizadas pela Secretaria de Ordem Pública. Áreas como
+        irregulares têm uma{" "}
+        <styles.TextBlue>incidência maior na Zona Oeste</styles.TextBlue> do
+        Município, onde ocorrem cerca de <styles.TextBlue>66%</styles.TextBlue>{" "}
+        das demolições realizadas pela Secretaria de Ordem Pública. Áreas como
         Recreio, Rio das Pedras e Muzema, por exemplo, são bairros extremamente
         afetados por essas irregularidades.
-      </IntroText>
+      </styles.IntroText>
     </styles.IntroDiv>
   );
 }
--- src/pages/demolicoes-seop/components/chapters.style.js
+++ src/pages/demolicoes-seop/components/chapters.style.js
@@ -1,64 +1,153 @@
 // CSS
+import { Container } from "postcss";
 import styled from "styled-components";
+import tw from "tailwind-styled-components";
 import capa from "../images/capa.gif";
 
-export const TextChapterIndicator = styled.div`
-  position: absolute;
-  background: #01baef;
-  text-align: "left";
-  font-size: 1.5rem;
-  color: #cfe795;
-  // opacity: 1;
-  z-index: 9999;
-  // top: 0;
-  // left: 0;
+export const CapaDiv = tw.div`
+  flex flex-col
+  h-screen w-full
+  items-center
+  pl-5 pr-5
+  lg:mr-20 lg:ml-20
+  sm:ml-30 sm:mr-30
+  sm:w
 `;
 
-export const CapaDiv = styled.div`
-  width: 100%;
-  height: 100vh;
-  z-index: -1;
-  display: grid;
-  grid-template-columns: 0.5fr 2fr 0.5fr;
-  grid-template-rows: 0.6fr 1.9fr 0.6fr 0.6fr 2fr 0.3fr;
-  gap: 10px 20px;
-  grid-template-areas:
-    ". . ."
-    ". logo ."
-    ". titulo ."
-    ". subtitulo ."
-    ". . ."
-    ". . .";
-  // grid-template-columns: ;
-  background-image: url(${capa});
-  background-size: cover;
-  justify-items: right;
+export const Title = tw.div`
+  font-roboto font-bold  
+  text-3xl text-center text-white    
+  mt-28
+  sm:mt-44
+  lg:text-4xl  
+  lg:mt-36
+  `;
+
+export const AuthorText = tw.div`
+  font-roboto
+  text-sm text-center text-white
+  mt-6
+  sm:mt-12
+  lg:text-base
+  lg:mt-5
 `;
 
-export const ChapterGenericDiv = styled.div`
-  height: 100vh;
-  width: 100%;
-
-  z-index: -1;
-  display: flex;
-  align-items: center;
+export const IntroTitle = tw.div`
+  font-bold  font-roboto
+  text-5xl text-right text-white
+  
+  mt-[120%]
 `;
 
-export const IntroDiv = styled.div`
-  height: 200vh;
-  width: 100%;
-  overflow-x: hidden;
-  display: grid;
-  grid-template-columns: 1fr 1.2fr 0.4fr;
-  grid-template-rows: 1fr 1fr 0.5fr 1.5fr 1fr;
-  gap: 30px 20px;
-  grid-template-areas:
-    ". . ."
-    ". imagem ."
-    ". titulo ."
-    ". texto ."
-    ". . .";
-  background-color: rgba(0, 0, 0, 0.4);
-  backdrop-filter: blur(8px);
-  opacity: 1;
+export const IntroText = tw.div`
+  font-roboto  
+  align-middle
+  text-xl text-justify text-white 
+  leading-normal 
+  opacity-100
+  mt-[120%]
+`;
+
+export const ChapterGenericDiv = tw.div`
+  flex
+  w-full h-screen 
+  items-center
+  text-white
+  -z-10
+`;
+
+export const IntroDiv = tw.div`
+  w-full h-200
+  gap-8
+  bg-opacity-40 backdrop-blur
+  flex items-start 
+  pr-[15%] pl-[15%]
+`;
+
+export const NewsImage = tw.img`
+w-full
+mb-auto
+mt-[2%]
+`;
+
+export const NewsTitle = tw.div`
+  text-white
+  font-roboto
+  text-4xl
+  mb-[15px]
+`;
+
+export const ContainerCard = tw.div`
+  m-auto
+  w-2/5
+  p-10
+  bg-opacity-70 backdrop-blur-sm
+
+`;
+
+export const ContainerCard1 = tw.div`
+  m-auto
+  w-2/5
+  p-10
+  bg-opacity-[20%] backdrop-blur-sm
+`;
+
+const TextCardSC = styled.div`
+  color: #dbdbdb;
+`;
+
+export const TextCard = tw(TextCardSC)`
+  font-roboto  
+  text-base
+  text-left
+`;
+
+export const TextCard1 = tw(TextCardSC)`
+  font-roboto  
+  text-base
+  text-left
+  mt-0
+  mb-0
+`;
+
+export const Grid1 = tw.div`
+  flex
+  gap-x-0
+  gap-y-5
+  items-end
+  justify-start
+  mb-5
+`;
+
+export const Grid = tw.div`
+  block
+  gap-x-0
+  gap-y-8
+`;
+
+const BigNumberSC = styled.div`
+  color: #1a374d;
+  font-family: "Redaction", sans-serif;
+  font-weight: 700;
+`;
+
+export const BigNumber = tw(BigNumberSC)`
+  font-roboto  
+  font-bold
+  text-9xl
+  text-left
+  self-end
+`;
+
+export const BigNumber1 = tw(BigNumber)`
+  text-7xl
+`;
+
+const TextBlueSC = styled.div`
+  background: #1a374d;
+`;
+
+export const TextBlue = tw(TextBlueSC)`
+  text-white
+  inline
 `;
