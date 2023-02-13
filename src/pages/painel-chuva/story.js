08be93c79ef4222c1e3ad0de4c6060e7b12ea7df
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-02-13 17:48:34 -0300

    fix: assert that hex is not null

--- src/pages/painel-chuva/story.js
+++ src/pages/painel-chuva/story.js
@@ -64,9 +64,10 @@ export default function PainelChuva() {
   const hexToRgb = (hex) => {
     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
     var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function (m, r, g, b) {
-      return r + r + g + g + b + b;
-    });
+    if (hex)
+      hex = hex.replace(shorthandRegex, function (m, r, g, b) {
+        return r + r + g + g + b + b;
+      });
 
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result
