3c9f409f98557ab163f1efb8ed54365222bea821
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-02-13 18:56:17 -0300

    fix: assert `hex` exists

--- src/pages/painel-chuva/story.js
+++ src/pages/painel-chuva/story.js
@@ -64,10 +64,13 @@ export default function PainelChuva() {
   const hexToRgb = (hex) => {
     // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
     var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    if (hex)
+    if (hex) {
       hex = hex.replace(shorthandRegex, function (m, r, g, b) {
         return r + r + g + g + b + b;
       });
+    } else {
+      hex = "#FFFFFF";
+    }
 
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result
