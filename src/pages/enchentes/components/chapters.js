b67a2e7f4b67160833a66ce63167ced3eea32d0c
Caio <caio.rodrigues.jacintho@gmail.com>
2023-06-05 14:10:20 -0300

    add shadow

--- src/pages/enchentes/components/chapters.js
+++ src/pages/enchentes/components/chapters.js
@@ -47,7 +47,7 @@ export function Capa(
           src={logo}
           className="absolute top-[5%] lg:absolute lg:top-[20px] h-11 lg:h-14"
         ></img> */}
-        <styles.Title>Por que o Rio de Janeiro alaga? </styles.Title>
+        <styles.Title >Por que o Rio de Janeiro alaga? </styles.Title>
         <styles.Subtitle>
           E como a prefeitura trabalha para que a chuva não castigue os cariocas
         </styles.Subtitle>
--- src/pages/enchentes/components/chapters.style.js
+++ src/pages/enchentes/components/chapters.style.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import tw from "tailwind-styled-components";
 import capa from "../images/capa.gif";
 
-export const CapaDivSC = styled.div`
+export const CapaDivSC = styled.div `
   background-color: #ffffff;
   background-image: url(${capa});
   background-size: 20%;
@@ -13,31 +13,32 @@ export const CapaDivSC = styled.div`
   /* filter: blur(8px); */
 `;
 
-export const CapaDiv = tw(CapaDivSC)`
+export const CapaDiv = tw(CapaDivSC)
+`
   h-[100vh] w-full
   flex flex-col
   justify-center  items-center
   bg-white
   `;
 
-export const CapaAutor = tw.div`
+export const CapaAutor = tw.div `
   flex flex-col  
   lg:h-auto lg:w-[800px]
   justify-center items-center
   `;
 
-export const Title = tw.div`  
+export const Title = tw.div `  
   font-geologica font-[700]
   text-[32px]
   text-center
   leading-[120%]
   p-2
   lg:text-[48px]
-  text-black
-  bg-white
+  text-white
+  drop-shadow-[0_5.2px_5.2px_rgba(0,0,0,0.8)]
     `;
 
-export const Subtitle = tw.div`
+export const Subtitle = tw.div `
 font-geologica font-[300]
 text-center
 text-[18px]
@@ -45,11 +46,11 @@ lg:text-[22px]
 leading-[120%]
 p-2
 mt-[12px]
-text-black
-bg-white
+text-white
+drop-shadow-[0_5.2px_5.2px_rgba(0,0,0,0.8)]
 `;
 
-export const Video = tw.div`
+export const Video = tw.div `
   flex flex-col  
   h-auto w-[56w]
   lg:h-[700px] lg:w-[700px]
@@ -59,19 +60,19 @@ export const Video = tw.div`
   
   `;
 
-export const Scroll = tw.div`
+export const Scroll = tw.div `
   flex lg:flex-row flex-col 
   lg:justify-between lg:items-center
   `;
 
-export const AuthorText = tw.div`
+export const AuthorText = tw.div `
   font-geologica
   text-[14px] text-left text-black
   leading-[150%]
   mb-[120px]
  `;
 
-export const ChapterGenericDiv = tw.div`
+export const ChapterGenericDiv = tw.div `
 flex
 w-full h-auto
 items-center
@@ -81,7 +82,7 @@ lg:p-[34px]
 bg-white
 `;
 
-export const ChapterGenericDiv1 = tw.div`
+export const ChapterGenericDiv1 = tw.div `
   flex
   w-full h-auto
   items-center
@@ -91,7 +92,7 @@ export const ChapterGenericDiv1 = tw.div`
   bg-white
   `;
 
-export const ChapterGenericDiv2 = tw.div`
+export const ChapterGenericDiv2 = tw.div `
     flex
     relative
     justify-center
@@ -103,14 +104,14 @@ export const ChapterGenericDiv2 = tw.div`
     bg-transparent	
   `;
 
-export const ChapterGenericDiv3 = tw.div`
+export const ChapterGenericDiv3 = tw.div `
 h-full w-full
 items-center
 justify-center
 bg-white
 `;
 
-export const ChapterGenericDiv4 = tw.div`
+export const ChapterGenericDiv4 = tw.div `
   flex
   w-full h-[100vh]
   items-center
@@ -120,20 +121,20 @@ export const ChapterGenericDiv4 = tw.div`
   bg-white
   `;
 
-export const ContainerCard = tw.div`
+export const ContainerCard = tw.div `
 h-auto w-[700px]
 mb-[60px]
 mt-[100px]
 `;
 
-export const ContainerCard2 = tw.div`
+export const ContainerCard2 = tw.div `
   h-auto w-[40vw]
   backdrop-blur-[50px]
   bg-[#00000099]
   p-[34px]
 `;
 
-export const ContainerCard3 = tw.div`
+export const ContainerCard3 = tw.div `
     absolute
     z-10
     h-auto w-[90%] lg:w-[600px]
@@ -145,7 +146,7 @@ export const ContainerCard3 = tw.div`
     rounded-lg
   `;
 
-export const IntroDiv = tw.div`
+export const IntroDiv = tw.div `
   w-full h-screen
   bg-opacity-40 backdrop-blur
   flex flex-col 
@@ -153,7 +154,7 @@ export const IntroDiv = tw.div`
   bg-black/50
 `;
 
-export const TextCreditos = tw.div`
+export const TextCreditos = tw.div `
   text-black
   font-geologica
   font-semibold
@@ -161,14 +162,14 @@ export const TextCreditos = tw.div`
   mb-[2px]
 `;
 
-export const TextCreditos1 = tw.div`
+export const TextCreditos1 = tw.div `
   text-black
   font-geologica
   font-regular
   text-[18px]
 `;
 
-export const TextCreditos2 = tw.div`
+export const TextCreditos2 = tw.div `
   text-black
   font-geologica
   font-black
@@ -177,7 +178,7 @@ export const TextCreditos2 = tw.div`
   mb-[40px]
 `;
 
-export const TextCard = tw.div`
+export const TextCard = tw.div `
   font-geologica font-bold 
   w-[70%]
   text-[24px]
@@ -189,7 +190,7 @@ export const TextCard = tw.div`
   mb-[16px]
 `;
 
-export const TextHighlightBlue = tw.div`
+export const TextHighlightBlue = tw.div `
   font-libre font-[400] 
   inline-block
   text-[14px]
@@ -201,7 +202,7 @@ export const TextHighlightBlue = tw.div`
   py-1
 `;
 
-export const TextHighlightBlueUm = tw.div`
+export const TextHighlightBlueUm = tw.div `
   font-libre font-[400] 
   inline-block
   text-[14px]
@@ -213,7 +214,7 @@ export const TextHighlightBlueUm = tw.div`
   py-1
 `;
 
-export const TextHighlightBlueDois = tw.div`
+export const TextHighlightBlueDois = tw.div `
   font-libre font-[400] 
   inline-block
   text-[14px]
@@ -226,7 +227,7 @@ export const TextHighlightBlueDois = tw.div`
 `;
 // selection:bg-[#ef4444] selection:text-white
 
-export const TextCard1 = tw.div`
+export const TextCard1 = tw.div `
 font-libre font-[400] 
 text-[14px]
 text-left
@@ -237,7 +238,7 @@ mb-[60px]
 lg:mb-0
 `;
 
-export const TextCard2 = tw.div`
+export const TextCard2 = tw.div `
 font-geologica font-[400] 
 text-[18px]
 text-left
@@ -246,7 +247,7 @@ text-bold
 mt-[20px]
 `;
 
-export const TextCard3 = tw.div`
+export const TextCard3 = tw.div `
 font-libre font-[400] 
 text-[14px]
 text-left
