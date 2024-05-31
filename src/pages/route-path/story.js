e304ea35efce7e94bb29310c8415616d43ccac57
d116626 <d116626@gmail.com>
2024-05-31 04:15:08 -0300

    chore: add trips chunks

--- src/pages/route-path/story.js
+++ src/pages/route-path/story.js
@@ -1,14 +1,11 @@
-// https://staging.api.civitas.dados.rio/cars/path?placa=SQZ8B08&start_time=2024-05-30%2021%3A34:09&end_time=2024-05-30%2022%3A34:09'
-// https://staging.api.civitas.dados.rio/cars/path?placa=SQZ8B08&start_time=2024-05-30%2000%3A00%3A00&end_time=2024-05-31%2000%3A00%3A00
-import { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import DeckGL from "@deck.gl/react";
 import { Map } from "react-map-gl";
 import mapboxgl from "mapbox-gl"; // do not remove this line
 import { GeoJsonLayer } from "@deck.gl/layers";
-import DATA from "./data/path_data.json";
-
-import DeckGL from "@deck.gl/react";
 import { Oval } from "react-loader-spinner";
 import { isMobile } from "react-device-detect";
+import DATA from "./data/path_data.json";
 
 // The following is required to stop "npm build" from transpiling mapbox code.
 // notice the exclamation point in the import.
@@ -26,9 +23,13 @@ export default function RoutePath() {
     zoom: isMobile ? 8.5 : 10.3,
   });
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({
+    locationsChunksGeojson: [],
+    polylineChunksGeojson: [],
+  });
   const [hoverInfo, setHoverInfo] = useState(null);
-  const [clickedChunkIndex, setClickedChunkIndex] = useState(null);
+  const [selectedTrip, setSelectedTrip] = useState(null);
+  const [selectedChunk, setSelectedChunk] = useState(null);
   const [placa, setPlaca] = useState("SQZ8B08");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -111,10 +112,14 @@ export default function RoutePath() {
 
       const result = await response.json();
       console.log("Data fetched:", result);
+      // console.log("Data local:", DATA);
+
       setData(result);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
+    // console.log("Data local:", DATA);
+    // setData(DATA);
   };
 
   const handleSubmit = (e) => {
@@ -125,96 +130,115 @@ export default function RoutePath() {
   };
 
   useEffect(() => {
-    if (data.length === 0) {
+    if (data.locationsChunksGeojson) {
       console.log("Initial data fetch");
       fetchData();
     }
   }, [data]);
 
+  const getLineLayerData = () => {
+    if (selectedTrip !== null) {
+      if (selectedChunk !== null) {
+        return {
+          type: "FeatureCollection",
+          features: [data.polylineChunksGeojson[selectedTrip][selectedChunk]],
+        };
+      } else {
+        return {
+          type: "FeatureCollection",
+          features: data.polylineChunksGeojson[selectedTrip],
+        };
+      }
+    } else {
+      return {
+        type: "FeatureCollection",
+        features: data.polylineChunksGeojson.flat(),
+      };
+    }
+  };
+
+  const getPointLayerData = () => {
+    if (selectedTrip !== null) {
+      if (selectedChunk !== null) {
+        return {
+          type: "FeatureCollection",
+          features:
+            data.locationsChunksGeojson[selectedTrip][selectedChunk].features,
+        };
+      } else {
+        return {
+          type: "FeatureCollection",
+          features: data.locationsChunksGeojson[selectedTrip]
+            .flat()
+            .map((chunk) => chunk.features)
+            .flat(),
+        };
+      }
+    } else {
+      return {
+        type: "FeatureCollection",
+        features: data.locationsChunksGeojson
+          .flat()
+          .map((chunk) => chunk.features)
+          .flat(),
+      };
+    }
+  };
+
   const layers = [];
 
-  if (clickedChunkIndex === null) {
-    // Render all layers if no chunk is clicked
+  // if data have polylineChunksGeojson then push the lineLayer to layers
+
+  if (data.polylineChunksGeojson) {
     layers.push(
       new GeoJsonLayer({
-        id: "path",
-        data: data.polylineChunksGeojson,
+        id: "geojson-line-layer",
+        data: getLineLayerData(),
         pickable: true,
         stroked: true,
-        filled: false,
-        lineWidthMinPixels: 3,
+        filled: true,
+        lineWidthScale: 2,
+        lineWidthMinPixels: 2,
         getLineColor: [255, 255, 255, 255],
-        onHover: (info) => setHoverInfo(info),
-        onClick: (info) => setClickedChunkIndex(info.index),
+        getRadius: 100,
+        getLineWidth: 5,
+        getElevation: 30,
+        onHover: setHoverInfo,
       })
     );
-    if (data.locationsChunksGeojson) {
-      data.locationsChunksGeojson.forEach((chunk, chunkIndex) => {
-        layers.push(
-          new GeoJsonLayer({
-            id: `locations-${chunkIndex}`,
-            data: chunk,
-            pickable: true,
-            stroked: false,
-            filled: true,
-            pointRadiusMinPixels: 10,
-            getFillColor: (d, { index }) => {
-              if (index === 0) {
-                return [0, 255, 0, 200]; // Green for the first location
-              } else if (index === chunk.features.length - 1) {
-                return [255, 0, 0, 200]; // Red for the last location
-              } else {
-                return [255, 140, 0, 200]; // Orange for other locations
-              }
-            },
-            onHover: (info) => setHoverInfo(info),
-            onClick: (info) => setClickedChunkIndex(chunkIndex),
-          })
-        );
-      });
-    }
-  } else {
-    // Render only the clicked chunk
+  }
+
+  if (data.locationsChunksGeojson) {
     layers.push(
       new GeoJsonLayer({
-        id: `path-${clickedChunkIndex}`,
-        data: data.polylineChunksGeojson[clickedChunkIndex],
+        id: "geojson-point-layer",
+        data: getPointLayerData(),
         pickable: true,
-        stroked: true,
-        filled: false,
-        lineWidthMinPixels: 3,
-        getFillColor: [0, 0, 0, 255],
-        onHover: (info) => setHoverInfo(info),
-        onClick: () => setClickedChunkIndex(null), // Reset on click
+        stroked: false,
+        filled: true,
+        getFillColor: (d, { index }) => {
+          const features = getPointLayerData().features;
+          if (index === 0) {
+            return [0, 255, 0, 200]; // Verde para o primeiro ponto
+          } else if (index === features.length - 1) {
+            return [255, 0, 0, 200]; // Vermelho para o último ponto
+          } else {
+            return [255, 165, 0, 200]; // Laranja para os demais pontos
+          }
+        },
+        getRadius: (d, { index }) => {
+          const features = getPointLayerData().features;
+          if (index === 0 || index === features.length - 1) {
+            return 6; // 2x o tamanho dos demais pontos
+          } else {
+            return 2; // Tamanho normal para os demais pontos
+          }
+        },
+        pointRadiusMinPixels: 2,
+        pointRadiusScale: 40,
+        onHover: setHoverInfo,
       })
     );
-    if (
-      data.locationsChunksGeojson &&
-      data.locationsChunksGeojson[clickedChunkIndex]
-    ) {
-      const chunk = data.locationsChunksGeojson[clickedChunkIndex];
-      layers.push(
-        new GeoJsonLayer({
-          id: `locations-${clickedChunkIndex}`,
-          data: chunk,
-          pickable: true,
-          stroked: false,
-          filled: true,
-          pointRadiusMinPixels: 10,
-          getFillColor: (d, { index }) => {
-            if (index === 0) {
-              return [0, 255, 0, 200]; // Green for the first location
-            } else if (index === chunk.features.length - 1) {
-              return [255, 0, 0, 200]; // Red for the last location
-            } else {
-              return [255, 140, 0, 200]; // Orange for other locations
-            }
-          },
-          onHover: (info) => setHoverInfo(info),
-          onClick: () => setClickedChunkIndex(null), // Reset on click
-        })
-      );
-    }
   }
 
   return (
@@ -281,84 +305,105 @@ export default function RoutePath() {
             />
           </label>
         </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
+        <div>
           <label>
-            Select Chunk Index:
+            Select Trip:
             <select
-              value={clickedChunkIndex !== null ? clickedChunkIndex : ""}
-              onChange={(e) =>
-                setClickedChunkIndex(
+              value={selectedTrip !== null ? selectedTrip : ""}
+              onChange={(e) => {
+                setSelectedTrip(
                   e.target.value === "" ? null : Number(e.target.value)
-                )
-              }
+                );
+                setSelectedChunk(null); // Reset chunk selection
+              }}
               style={{ marginLeft: "10px" }}
             >
-              <option value="">All chunks</option>
+              <option value="">All Trips</option>
               {data.polylineChunksGeojson &&
                 data.polylineChunksGeojson.map((_, index) => (
                   <option key={index} value={index}>
-                    Chunk {index}
+                    Trip {index}
                   </option>
                 ))}
             </select>
           </label>
         </div>
+        {selectedTrip !== null && (
+          <div>
+            <label>
+              Select Chunk:
+              <select
+                value={selectedChunk !== null ? selectedChunk : ""}
+                onChange={(e) =>
+                  setSelectedChunk(
+                    e.target.value === "" ? null : Number(e.target.value)
+                  )
+                }
+                style={{ marginLeft: "10px" }}
+              >
+                <option value="">All Chunks</option>
+                {data.polylineChunksGeojson[selectedTrip] &&
+                  data.polylineChunksGeojson[selectedTrip].map((_, index) => (
+                    <option key={index} value={index}>
+                      Chunk {index}
+                    </option>
+                  ))}
+              </select>
+            </label>
+          </div>
+        )}
         <button type="submit">Fetch Data</button>
       </form>
-      {data && data.length === 0 && (
-        <div
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-          }}
-        >
-          <Oval height={100} width={100} color="#00BFFF" visible={true} />
-        </div>
-      )}
-      {data && (
-        <DeckGL
-          ref={mapRef}
-          initialViewState={viewport}
-          controller={true}
-          onViewportChange={({ target }) => {
-            setViewport(target);
-          }}
-          layers={layers}
-          style={{ position: "relative", height: "calc(100vh - 100px)" }} // Adjust map height
-        >
-          <Map
-            style={{ width: "100vw", height: "100%" }}
-            mapStyle="mapbox://styles/escritoriodedados/clgfevcvc009101p9ax017bah"
-            mapboxAccessToken="pk.eyJ1IjoiZXNjcml0b3Jpb2RlZGFkb3MiLCJhIjoiY2t3bWdmcHpjMmJ2cTJucWJ4MGQ1Mm1kbiJ9.4hHJX-1pSevYoBbja7Pq4w"
-          />
-          {hoverInfo && hoverInfo.object && (
-            <div
-              style={{
-                position: "absolute",
-                zIndex: 1,
-                pointerEvents: "none",
-                left: hoverInfo.x,
-                top: hoverInfo.y,
-                backgroundColor: "white",
-                padding: "5px",
-                borderRadius: "3px",
-                boxShadow: "0 2px 5px rgba(0, 0, 0, 0.3)",
-              }}
-            >
-              <div>
-                {Object.keys(hoverInfo.object.properties).map((key) => (
-                  <div key={key}>
-                    <strong>{key}: </strong>
-                    {hoverInfo.object.properties[key]}
-                  </div>
-                ))}
-              </div>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Oval height={100} width={100} color="#00BFFF" visible={true} />
+      </div>
+      <DeckGL
+        ref={mapRef}
+        initialViewState={viewport}
+        controller={true}
+        onViewportChange={({ target }) => {
+          setViewport(target);
+        }}
+        layers={layers}
+        style={{ position: "relative", height: "calc(100vh - 100px)" }} // Adjust map height
+      >
+        <Map
+          style={{ width: "100vw", height: "100%" }}
+          mapStyle="mapbox://styles/escritoriodedados/clgfevcvc009101p9ax017bah"
+          mapboxAccessToken="pk.eyJ1IjoiZXNjcml0b3Jpb2RlZGFkb3MiLCJhIjoiY2t3bWdmcHpjMmJ2cTJucWJ4MGQ1Mm1kbiJ9.4hHJX-1pSevYoBbja7Pq4w"
+        />
+        {hoverInfo && hoverInfo.object && (
+          <div
+            style={{
+              position: "absolute",
+              zIndex: 1,
+              pointerEvents: "none",
+              left: hoverInfo.x,
+              top: hoverInfo.y,
+              backgroundColor: "white",
+              padding: "5px",
+              borderRadius: "3px",
+              boxShadow: "0 2px 5px rgba(0, 0, 0, 0.3)",
+            }}
+          >
+            <div>
+              {Object.keys(hoverInfo.object.properties).map((key) => (
+                <div key={key}>
+                  <strong>{key}: </strong>
+                  {hoverInfo.object.properties[key]}
+                </div>
+              ))}
             </div>
-          )}
-        </DeckGL>
-      )}
+          </div>
+        )}
+      </DeckGL>
     </div>
   );
 }
