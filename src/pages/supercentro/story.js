e93aa0f6e4fa50cd35f09c298d7a1b60610279b4
Lucas Tavares <lucastavarestt@gmail.com>
2024-11-26 17:08:12 -0300

    fix

--- src/pages/supercentro/story.js
+++ src/pages/supercentro/story.js
@@ -8,7 +8,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function SuperCentro() {
   const videoRef = useRef(null);
-  const totalFrames = 24 * videoRef.current?.duration || 0; // 24 FPS * video duration
   const videoScrollRef = useRef(null);
 
   useEffect(() => {
@@ -17,10 +16,10 @@ export default function SuperCentro() {
 
       // Wait until the video metadata is loaded to get the duration
       video.addEventListener("loadedmetadata", () => {
-        const totalFrames = 48 * video.duration;
+        const totalFrames = 24 * video.duration;
 
         gsap.to(video, {
-          currentTime: totalFrames / 48,
+          currentTime: totalFrames / 24,
           scrollTrigger: {
             trigger: videoScrollRef.current,
             start: "top top",
@@ -29,7 +28,7 @@ export default function SuperCentro() {
             onUpdate: (self) => {
               // Calculate the frame based on scroll progress
               const frame = Math.round(self.progress * totalFrames);
-              video.currentTime = frame / 48; // Set the current video time
+              video.currentTime = frame / 24; // Set the current video time
             },
           },
         });
